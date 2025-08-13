202092e3c0cd165a96b55848331c8645df7d3e90
Mauricio Coria <coriamauricio@gmail.com>
2025-08-13 00:45:36 -0300

    Add `Serializable` implementation to `MatchResult` record for better object serialization support.

--- uci-arena/src/main/java/net/chesstango/uci/arena/MatchResult.java
+++ uci-arena/src/main/java/net/chesstango/uci/arena/MatchResult.java
@@ -3,10 +3,11 @@ package net.chesstango.uci.arena;
 import net.chesstango.gardel.pgn.PGN;
 import net.chesstango.search.SearchResult;
 
+import java.io.Serializable;
 import java.util.List;
 
 /**
  * @author Mauricio Coria
  */
-public record MatchResult(String mathId, PGN pgn, List<SearchResult> whiteSearches, List<SearchResult> blackSearches) {
+public record MatchResult(String mathId, PGN pgn, List<SearchResult> whiteSearches, List<SearchResult> blackSearches) implements Serializable {
 }
